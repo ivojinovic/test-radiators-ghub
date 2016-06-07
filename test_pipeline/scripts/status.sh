1836e04f5415be722f21e0f4790c7fa3b0f465b4
Ivan Vojinovic <ivan.vojinovic@hs2solutions.com>
2016-06-07 02:05:12 -0400

    Adds pipeline_job_resource

--- test_pipeline/scripts/status.sh
+++ test_pipeline/scripts/status.sh
@@ -6,7 +6,7 @@ echo "STARTING STATUS CHECK"
 
 cd pipeline_job_resource
 
-job_file_name="jarvis_api.static-analysis-dev-branches.txt"
+job_file_name="$PIPELINE.$JOB.txt"
 cat $job_file_name
 
 line=$(head -n 1 $job_file_name)
